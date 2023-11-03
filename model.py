0fb97724985af79aa1565c4bc52f09d3d29502aa
phongnguyenai <nttphong2142@gmail.com>
2023-11-03 14:18:04 +1100

    First commit

---
+++ model.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+from modules import *
+import torch.nn.functional as F
+
+class Encoder(nn.Module):
+    def __init__(self, block, blocks, c=3):
+        super(Encoder, self).__init__()
+        self.c = c
+        self.in_planes, self.planes = c, [32, 64, 128, 256, 512]
+        fpn_planes, fpnhead_planes, share_planes = 128, 64, 8
+        stride, nsample = [1, 4, 4, 4, 4], [8, 16, 16, 16, 16]
+        self.enc1 = self.builder(block, self.planes[0], blocks[0], share_planes, stride=stride[0], nsample=nsample[0])  # N/1
+        self.enc2 = self.builder(block, self.planes[1], blocks[1], share_planes, stride=stride[1], nsample=nsample[1])  # N/4
+        self.enc3 = self.builder(block, self.planes[2], blocks[2], share_planes, stride=stride[2], nsample=nsample[2])  # N/16
+        self.enc4 = self.builder(block, self.planes[3], blocks[3], share_planes, stride=stride[3], nsample=nsample[3])  # N/64
+        self.enc5 = self.builder(block, self.planes[4], blocks[4], share_planes, stride=stride[4], nsample=nsample[4])  # N/256
+        self.linear = nn.Sequential(nn.Linear(self.planes[4], self.planes[4]), nn.BatchNorm1d(self.planes[4]), nn.ReLU(inplace=True), nn.Linear(self.planes[4], self.planes[4]))
+
+    def builder(self, block, planes, blocks, share_planes=8, stride=1, nsample=16):
+        layers = []
+        layers.append(DownSampler(self.in_planes, planes * block.expansion, stride, nsample))
+        self.in_planes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_planes, self.in_planes, share_planes, nsample=nsample))
+        return nn.Sequential(*layers)
+
+    def forward(self, pxo, batch):
+        p0, x0, o0 = pxo  # (n, 3), (n, c), (b)
+        x0 = p0 if self.c == 3 else torch.cat((p0, x0), 1)
+        p1, x1, o1 = self.enc1([p0, x0, o0])
+        p2, x2, o2 = self.enc2([p1, x1, o1])
+        p3, x3, o3 = self.enc3([p2, x2, o2])
+        p4, x4, o4 = self.enc4([p3, x3, o3])
+        p5, x5, o5 = self.enc5([p4, x4, o4])
+            
+        x5 = x5.reshape(batch, -1, self.planes[-1])
+        x5 = torch.max(x5, dim=1)[0]
+        
+        return x5
+
+class Decoder(nn.Module):
+    def __init__(self, up_factors, dim_feat=512, num_pc=512, num_p0=512,
+                 radius=1, bounding=True):
+        super(Decoder, self).__init__()
+        self.num_p0 = num_p0           
+        self.decoder_coarse = SeedGenerator(dim_feat=dim_feat, num_pc=num_pc)              
+        
+        uppers = []
+        for i, factor in enumerate(up_factors):
+            uppers.append(SPD(dim_feat=dim_feat, up_factor=factor, i=i, bounding=bounding, radius=radius))
+
+        self.uppers = nn.ModuleList(uppers)
+
+    def forward(self, feat, partial = None, return_P0=False):
+        """
+        Args:
+            feat: Tensor, (b, dim_feat, n)
+            partial: Tensor, (b, n, 3)
+        """
+        arr_pcd = []
+        pcd = self.decoder_coarse(feat).contiguous()  # (B, num_pc, 3)
+        arr_pcd.append(pcd)
+
+        pcd = fps_subsample(torch.cat([pcd, partial], 1), self.num_p0)     
+        
+        if return_P0:
+            arr_pcd.append(pcd)
+        K_prev = None
+        pcd = pcd.permute(0, 2, 1).contiguous()
+        for upper in self.uppers:
+            pcd, K_prev = upper(pcd, feat, K_prev)
+            arr_pcd.append(pcd.permute(0, 2, 1).contiguous())
+
+        return arr_pcd
+
+class ATT_Net(nn.Module):
+    def __init__(self):
+        super(ATT_Net, self).__init__()
+        self.encoder = Encoder(AttentionBlock, [2, 3, 4, 6, 3]).cuda()
+        self.decoder = Decoder(dim_feat=512, num_pc=256, num_p0=512,
+                               radius=1, bounding=True, up_factors=[4,1,4]).cuda()
+        
+    def forward(self, partial, batch_size):
+        pp, xp, op = partial.pos, partial.pos, partial.ptr[1:] 
+        
+        encoding = self.encoder([pp, xp, op], batch_size)
+        complete = self.decoder(encoding.unsqueeze(2), partial = partial.pos.reshape(batch_size,-1,3))[-1]
+        
+        return complete
+            
---
+++ train.py
@@ -0,0 +1,191 @@
+from __future__ import print_function
+import os
+import random
+import numpy as np
+import torch
+from torch_geometric.data import DataLoader
+from model import ATT_Net
+
+import sys
+sys.path.append("../../IEEE-Sensor-2023/code/")
+
+from datasets import PCNDataset
+from pytorch3d.loss.chamfer import chamfer_distance
+import argparse
+import sys
+from torch.optim.lr_scheduler import StepLR
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", help="train or test", default="train")
+parser.add_argument("--pretrained", help="pth file", default="")
+parser.add_argument("--car", help="True or False", default=True)
+parser.add_argument("--batch-size", help="Batch size", default=8)
+parser.add_argument("--model-name", help="Name of the method", default="ATT-Net")
+parser.add_argument("--epoch", help="Epoch", default=401)
+parser.add_argument("--num-pred", help="Number of output point cloud", default=8192)
+
+args = parser.parse_args()
+
+class Config( object ):
+    def __init__( self ):
+        self.myAttr= None
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    count = 0
+    for data in train_dataloader:
+        gt = data[3].to(device)
+        partial = data[2].to(device)
+        batch = partial.pos.reshape(-1, 2048, 3).shape[0]
+        
+        # Prediction
+        optimizer.zero_grad()
+        
+        pc = model(partial, batch)
+        loss = chamfer_distance(pc, gt.pos.reshape(batch,-1,3), norm = 2)[0]*1000
+        
+        torch.cuda.empty_cache()
+        loss.backward()
+        train_loss += loss.item() * batch
+        optimizer.step()
+        count +=1
+        print(f"{model_name} - Training epoch {epoch}: {int(count/len(train_dataloader)*100)}%", end='\r')
+        sys.stdout.flush()
+        
+    return train_loss / len(train_dataset) / 1000
+
+def evaluation():
+    model.eval()
+    val_loss = 0
+    
+    for data in val_dataloader:      
+        gt = data[3].to(device)
+        partial = data[2].to(device)
+        batch = partial.pos.reshape(-1, 2048, 3).shape[0]
+        
+        # Prediction
+        with torch.no_grad():
+            pc = model(partial, batch)
+            chamfer_loss = chamfer_distance(pc, gt.pos.reshape(batch,-1,3), norm = 2)[0]
+            val_loss += chamfer_loss.item() * batch
+            
+    return val_loss/len(val_dataset)
+
+def test():
+    model.eval()
+    test_loss = 0
+    
+    for data in test_dataloader:      
+        gt = data[3].to(device)
+        partial = data[2].to(device)
+        batch = partial.pos.reshape(-1, 2048, 3).shape[0]
+        
+        # Prediction
+        with torch.no_grad():
+            pc = model(partial, batch)
+            chamfer_loss = chamfer_distance(pc, gt.pos.reshape(batch,-1,3), norm = 2)[0]
+            test_loss += chamfer_loss.item() * batch
+            
+    return test_loss/len(test_dataset)
+
+seed_everything()
+
+# Train cofig
+train_config = Config()
+train_config.subset = "train"
+train_config.PARTIAL_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{train_config.subset}/partial"
+train_config.COMPLETE_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{train_config.subset}/complete"
+train_config.CATEGORY_FILE_PATH = "../../IEEE-Sensor-2023/code/PCN/PCN.json"
+train_config.N_POINTS = int(args.num_pred)
+train_config.CARS = False
+
+# Valid cofig
+val_config = Config()
+val_config.subset = "val"
+val_config.PARTIAL_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{val_config.subset}/partial"
+val_config.COMPLETE_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{val_config.subset}/complete"
+val_config.CATEGORY_FILE_PATH = "../../IEEE-Sensor-2023/code/PCN/PCN.json"
+val_config.N_POINTS = int(args.num_pred)
+val_config.CARS = False
+
+# Test cofig
+test_config = Config()
+test_config.subset = "test"
+test_config.PARTIAL_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{test_config.subset}/partial"
+test_config.COMPLETE_POINTS_PATH = f"../../IEEE-Sensor-2023/code/PCN/{test_config.subset}/complete"
+test_config.CATEGORY_FILE_PATH = "../../IEEE-Sensor-2023/code/PCN/PCN.json"
+test_config.N_POINTS = int(args.num_pred)
+test_config.CARS = False
+
+# Dataset
+train_dataset = PCNDataset.PCN(train_config)
+val_dataset = PCNDataset.PCN(val_config)
+test_dataset = PCNDataset.PCN(test_config)
+
+# Dataloader
+batch_size = int(args.batch_size)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                    shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size,
+                    shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
+                    shuffle=False)
+
+# Load model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = ATT_Net().to(device)
+
+# optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00007)
+
+# Model name
+model_name = args.model_name
+
+# torch.cuda.empty_cache()
+# torch.set_num_threads(24)
+
+min_loss = 99999999999999
+
+path = f"weights/{model_name}"
+if not os.path.exists(path):
+    os.makedirs(path)
+    
+if args.mode == "train":
+    scheduler = StepLR(optimizer, step_size=40, gamma=0.7)
+    # Start training
+    for epoch in range(int(args.epoch)):
+        train_loss = train(epoch)
+        eval_loss = evaluation()
+            
+        f = open(f'{path}/log_loss.txt', 'a')
+        str_loss = "Epoch {:03d} - Training loss: {:.10f} - Validation loss: {:.10f} \n".format(epoch, train_loss, eval_loss)
+        f.write(str_loss)
+        print(str_loss)
+
+        if eval_loss < min_loss:
+            torch.save(model.state_dict(),f'./{path}/best.pt')
+            min_loss = eval_loss
+
+        torch.save(model.state_dict(),f'./{path}/last.pt')
+        scheduler.step()
+
+    # Print results
+    torch.cuda.empty_cache()
+    model = ATT_Net().to(device)
+    model.load_state_dict(torch.load(f"{path}/best.pt"))
+    test_loss = test()
+
+    str_test = "Test loss: {:.10f}".format(test_loss)
+    f.write(str_test)
+    print(str_test)
+
+    
